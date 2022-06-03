718f8fdb10e13c7c5754ce00e4aff36bd96d7ffd
Viktor <vivchy79@gmail.com>
2022-06-03 17:30:50 +0300

    add delete

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a really really really really long secret key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db'
 
 db = SQLAlchemy(app)
+def slug_translator(text):
+    slug = ''
+    word_translate = {"й":"i", "ц":"c", "у":"u", "к":"k", "е":"e", "н":"n",
+                      "г":"g", "ш":"sh", "щ":"shj","з":"z", "х":"h", "ъ":"j",
+                      "ф":"f","ы":"i","в":"v", "а":"a", "п": "p", "р":"r", "о":"o",
+                      "л":"l", "д":"d", "ж":"j","э":"e","я":"ya","ч":"ch","с":"s",
+                      "м":"m", "и":"i","т":"t","ь":"j", "б":"b", "ю":"yu", " ": "_"}
+    for w in text:
+        word = w.lower()
+        if word in word_translate.keys():
+            slug += word_translate[word]
+    return slug
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -26,13 +39,41 @@ def index():
 
 @app.route('/create', methods=['POST', 'GET'])
 def create():
-    print(request.form['slug'])
-    for i in request.form.keys():
-        print(i)
+    if request.method == 'POST':
+
+        try:
+            title = request.form['title']
+            slug = slug_translator(title)
+            content = request.form['content']
+            p = Post(title=title, slug=slug, content=content)
+            db.session.add(p)
+            db.session.commit()
+            print(p.title)
+            return render_template('create.html', add_post = 'запись добавлена')
+        except Exception as e:
+            print(e)
     return render_template('create.html')
 
+@app.route('/posts')
+def posts():
+    posts = db.session.query(Post).all()
+    print(posts)
 
+    return render_template('posts.html', posts=posts)
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    delete_post = db.session.query(Post).get(id)
+    db.session.delete(delete_post)
+    db.session.commit()
+    return redirect(url_for('posts'))
+
+@app.route('/post/<slug>')
+def delete(id):
+    delete_post = db.session.query(Post).get(id)
+    db.session.delete(delete_post)
+    db.session.commit()
+    return redirect(url_for('posts'))
 
 if __name__ == "__main__":
     app.run(debug=True)
